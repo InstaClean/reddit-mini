f735fde3cc3c67643a448414f3aecfbacceaccff
Kevin <kbeens@protonmail.com>
2021-09-09 08:53:44 -0700

    added buttons

--- src/features/subreddits/subreddits.tsx
+++ src/features/subreddits/subreddits.tsx
@@ -1,9 +1,13 @@
 export const Subreddits = () => {
     return (
         <div className="subreddits" >
-            <h2>
-                subreddits go here
-            </h2>
+            <ul>
+                <li><button>r/Popular</button></li>
+                <li><button>r/cryptoCurrency</button></li>
+                <li><button>r/pics</button></li>
+                <li><button>r/news</button></li>
+                <li><button>r/memes</button></li>
+            </ul>
         </div>
 
     )
